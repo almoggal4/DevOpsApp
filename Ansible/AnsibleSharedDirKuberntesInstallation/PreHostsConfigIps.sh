35d68d0e5a245ebca14627cafa27884a31fc146c
Almog Gal <almoggal11@gmail.com>
2024-01-21 13:00:29 +0200

    interview

--- Ansible/AnsibleSharedDirKuberntesInstallation/PreHostsConfigIps.sh
+++ Ansible/AnsibleSharedDirKuberntesInstallation/PreHostsConfigIps.sh
@@ -1,9 +1,11 @@
 # /bin/bash
-# create an host file dynamiclly based on the ip given by aws
+# create an host file dynamiclly based on the ip given by aws ang giving the key file the write permissions
+private_file_path=../PrivateFiles/kubernetes-managment-key-pair-private.pem
 instance_ip=`aws ec2 describe-addresses --filters "Name=tag:kubernetes.io/cluster/kubernetes,Values=owned" | grep '"PublicIp":' | awk -F : '{print $2}' | tr -d '" ,'`
 printf '%s\n' 'kubernetes-servers:'\
     "  hosts:" \
     "    master:"\
     "      ansible_host: $instance_ip" \
     "      ansible_connection: ssh"\
-    "      ansible_ssh_private_key_file: ../PrivateFiles/kubernetes-managment-key-pair-private.pem" >./hosts
+    "      ansible_ssh_private_key_file: $private_file_path" >./hosts
+chmod 700 $private_file_path
