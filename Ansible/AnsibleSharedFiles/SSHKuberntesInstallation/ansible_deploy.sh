bc85c6de9f2704e76ee2413fd35e0915bffb1549
Almog Gal <almoggal11@gmail.com>
2024-02-17 21:44:22 +0200

    jenkinsfile

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -1,6 +1,8 @@
 #ansible-playbook playbook.yml -i ./hosts
+kubernetes_server_public_ip=$(cat /root/terraform_ec2_public_ip)
+echo "$kubernetes_server_public_ip"
 echo "[kubernetes-servers]" > ./hosts
-echo "master ansible_host=3.74.189.204 ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem" >> ./hosts
+echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
 # get the ip externally - not hard coded
 # check that the ansible playbook is valid - validate
 # run the playbook 
--- TerraformAWS/TerraformSharedFiles/TerraformApp/terraform_deploy.sh
+++ TerraformAWS/TerraformSharedFiles/TerraformApp/terraform_deploy.sh
@@ -1,20 +1,18 @@
 #!/bin/bash
-#terraform init
-# Run your command and capture its output
+
+terraform init
 validate_output=$(terraform validate)
 
 # Define the expected value
 expected_value="Success!"
-
-echo "$validate_output"
 # Check if terraform configuration is valid
 if [[ "$validate_output" == *"$expected_value"* ]]; then
-    #sudo terraform apply -auto-approve
+    # apply the validate configuration
+    sudo terraform apply -auto-approve
     # save the public ip of the ec2 for later use
-    terraform output
-    #terraform_output_ip=$(terraform output)
-    #ec2_created_public_ip="$(cut -d' ' -f3 <<<"$terraform_output_ip")"
-    #echo "$ec2_created_public_ip" | sed s/'"'//
+    terraform_output_ip=$(terraform output)
+    ec2_created_public_ip="$(cut -d' ' -f3 <<<"$terraform_output_ip")"
+    echo "$ec2_created_public_ip" | sed s/'"'//g
 else
     echo "Terraform validate has failed, check the configuration of the terraform files."
     exit 1
