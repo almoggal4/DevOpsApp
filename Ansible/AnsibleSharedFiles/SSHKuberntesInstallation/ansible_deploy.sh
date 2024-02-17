b0cf60171075cc1f9219602506d709b5ffd29837
Almog Gal <almoggal11@gmail.com>
2024-02-17 21:58:32 +0200

    jenkinsfile

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -1,8 +1,6 @@
 #ansible-playbook playbook.yml -i ./hosts
-kubernetes_server_public_ip=$(cat /root/terraform_ec2_public_ip)
-echo "$kubernetes_server_public_ip"
+kubernetes_server_public_ip=$1
 echo "[kubernetes-servers]" > ./hosts
 echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
-# get the ip externally - not hard coded
 # check that the ansible playbook is valid - validate
 # run the playbook 
