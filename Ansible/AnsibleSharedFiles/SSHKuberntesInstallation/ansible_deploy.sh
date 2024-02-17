96275da162375bb287f948b5b8eaf963addccb7f
Almog Gal <almoggal11@gmail.com>
2024-02-17 22:08:14 +0200

    jenkinsfile

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -1,6 +1,6 @@
 #ansible-playbook playbook.yml -i ./hosts
 kubernetes_server_public_ip=$1
-echo "[kubernetes-servers]" > ./hosts
-echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
+sudo echo "[kubernetes-servers]" > ./hosts
+sudo echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
 # check that the ansible playbook is valid - validate
 # run the playbook 
