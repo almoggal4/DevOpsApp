0b852b46f143ef5f7dc5aeeeea329495e46ea37f
Almog Gal <almoggal11@gmail.com>
2024-03-09 20:22:27 +0200

    hello

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -1,7 +1,21 @@
-#ansible-playbook playbook.yml -i ./hosts
 kubernetes_server_public_ip=$(cat ./ec2_public_ip)
 sudo echo "[kubernetes-servers]" > ./hosts
 sudo echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
-# get the ip externally - not hard coded
-# check that the ansible playbook is valid - validate
-# run the playbook 
+# Check the syntax of the playbook
+ansible-playbook ./playbook.yml --syntax-check
+
+valid_syntax=$?
+
+if [ $valid_syntax -eq 0 ]; then
+    echo "Playbook syntax is valid."
+
+    # Run the playbook in check mode
+    ansible-playbook playbook.yml --check
+    valid_playbook=$?
+
+    if [ $valid_playbook -eq 0 ]; then
+        echo "Playbook is valid and can be executed."
+        ansible-playbook playbook.yml -i ./hosts
+    else
+        echo "Playbook is invalid or contains errors."
+    fi
--- GitHub/temp.py
+++ GitHub/temp.py
@@ -1,33 +1,34 @@
 import requests
+from lxml import etree
+from bs4 import BeautifulSoup
 
 # Jenkins API credentials and base URL
 USERNAME = 'root'
 PASSWORD = 'Lihialm1!'
 JENKINS_URL = 'http://localhost:8080'
+NEW_GITHUB_HOOK_URL = r'https://4de5-87-68-246-54.ngrok-free.app/github-webhook/'
 
-# # API endpoint for system configuration
-# SYSTEM_CONFIG_URL = f"{JENKINS_URL}/systemConfig/"
-#
-# # XML configuration for system settings (example)
-# system_config_xml = """
-# <jenkins>
-#     <numExecutors>2</numExecutors>
-#     <!-- Other system configuration settings go here -->
-# </jenkins>
-# """
-#
-# # Update system configuration
-#
-# headers = {'Content-Type': 'application/xml'}
-# response = requests.post(SYSTEM_CONFIG_URL,
-#                          auth=(USERNAME, PASSWORD),
-#                          headers=headers,
-#                          data=system_config_xml)
-#
-# if response.status_code == 200:
-#     print("System configuration updated successfully.")
-# else:
-#     print(f"Failed to update system configuration. Status code: {response.status_code}")
 SYSTEM_CONFIG_URL = f"{JENKINS_URL}/systemConfig.xml"
 response = requests.get(SYSTEM_CONFIG_URL, auth=(USERNAME, PASSWORD))
-print(response.text)
+xml_content = response.content.decode()
+xml_content = xml_content.replace('<link ', '<link />')
+xml_content = xml_content.replace('<meta ', '<meta />')
+xml_content = xml_content.replace('<link ', '<link />')
+xml_content = xml_content.replace('<script ', '<script />')
+soup = BeautifulSoup(xml_content, 'xml')
+github_hook_url = soup.find('githubWebHook')
+if github_hook_url:
+    github_hook_url['url'] = NEW_GITHUB_HOOK_URL
+else:
+    triggers_tag = soup.find('triggers')
+    if not triggers_tag:
+        triggers_tag = soup.new_tag('triggers')
+        soup.append(triggers_tag)  # Append 'triggers' to the root element
+    new_github_hook = soup.new_tag('githubWebHook', url=NEW_GITHUB_HOOK_URL)
+    triggers_tag.append(new_github_hook)
+modified_xml = str(soup)
+print(modified_xml)
+temp = requests.post(SYSTEM_CONFIG_URL, auth=(USERNAME, PASSWORD), data=modified_xml)
+print(temp)
+response = requests.get(SYSTEM_CONFIG_URL, auth=(USERNAME, PASSWORD))
+print(response)
