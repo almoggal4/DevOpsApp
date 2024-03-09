b2b0642f8e17892dadfe517e3a877cb7b10126e7
Almog Gal <almoggal11@gmail.com>
2024-03-09 20:57:56 +0200

    hello3

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -4,7 +4,7 @@ sudo echo "[kubernetes-servers]" > ./hosts
 sudo echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/root/PrivateFiles/kubernetes-managment-key-pair-private.pem' >> ./hosts
 
 # Check the syntax of the playbook
-ansible-playbook ./playbook.yml --syntax-check
+ansible-playbook playbook.yml --syntax-check
 
 valid_syntax=$?
 
