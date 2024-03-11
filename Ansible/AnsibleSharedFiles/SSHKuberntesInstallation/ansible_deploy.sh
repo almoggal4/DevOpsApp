2c0f30d1c61f3c8edffe727e7efb9a37efde27dd
Almog Gal <almoggal11@gmail.com>
2024-03-11 19:52:53 +0200

    hello

--- Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
+++ Ansible/AnsibleSharedFiles/SSHKuberntesInstallation/ansible_deploy.sh
@@ -3,24 +3,24 @@ kubernetes_server_public_ip=$(cat ./ec2_public_ip)
 sudo echo "[kubernetes-servers]" > ./hosts
 sudo echo 'master ansible_host='$kubernetes_server_public_ip 'ansible_connection=ssh ansible_ssh_private_key_file=/home/jenkins-remote-user/SSHPEM/ec2-kubernetes.pem' >> ./hosts
 
-# Check the syntax of the playbook
-ansible-playbook playbook.yml --syntax-check
+# # Check the syntax of the playbook
+# ansible-playbook playbook.yml --syntax-check
 
-valid_syntax=$?
+# valid_syntax=$?
 
-if [ $valid_syntax -eq 0 ]; then
-    echo "Playbook syntax is valid."
+# if [ $valid_syntax -eq 0 ]; then
+#     echo "Playbook syntax is valid."
 
-    # Run the playbook in check mode
-    ansible-playbook playbook.yml --check
-    valid_playbook=$?
+#     # Run the playbook in check mode
+#     ansible-playbook playbook.yml --check
+#     valid_playbook=$?
 
-    if [ $valid_playbook -eq 0 ]; then
-        echo "Playbook is valid and can be executed."
-        # run the ansible playbook to install k8s on the ec2 (using the udated host file)
-        ansible-playbook playbook.yml -i ./hosts
-    else
-        echo "Playbook contains errors."
-    fi
-fi
-echo "Playbook is invalid"
+#     if [ $valid_playbook -eq 0 ]; then
+#         echo "Playbook is valid and can be executed."
+#         # run the ansible playbook to install k8s on the ec2 (using the udated host file)
+#         ansible-playbook playbook.yml -i /home/root/AnsibleSharedFiles/SSHKuberntesInstallation/hosts
+#     else
+#         echo "Playbook contains errors."
+#     fi
+# fi
+# echo "Playbook is invalid"
